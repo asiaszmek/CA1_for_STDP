ba2ae69e80ccca5cfced7c7d1162c578883d1081
Joanna Jędrzejewska-Szmek <asia@in.waw.pl>
2021-04-23 14:11:48 +0200

    Make heatmaps of parameters on the whole dendritic tree

--- basal.py
+++ basal.py
@@ -8,9 +8,9 @@ from neuron import h
 
 if __name__ == "__main__":
     add_ER = False
-    where_spines = []  # ["CA1_PC_Tomko[0].radTprox1"]
+    where_spines = ["radTprox1"]
     where_ca = ["apical"]
-    t_stop = 10000
+    t_stop = 100
     h.load_file("stdrun.hoc")
     h.CVode()
     h.CVode().active(True)
@@ -19,30 +19,15 @@ if __name__ == "__main__":
     h.finitialize(-65)
     cell = CA1_PC(add_ER=add_ER, where_ca=where_ca, where_spines=where_spines)
     h.CVode().re_init()
-    
-    #f = open("python_model_3", "w")
-    # for sec in cell.sections:
-    #     f.write("%s\n" % sec.name())
-    #     for key in sorted(sec.psection()):
-    #         if isinstance(sec.psection()[key], dict):
-    #             for key2 in sec.psection()[key]:
-    #                 if isinstance(sec.psection()[key][key2], dict):
-    #                     for key3 in sec.psection()[key][key2].keys():
-    #                         f.write("%s, %s\n" % (key3,
-    #                                               sec.psection()[key][key2][key3]))
-    #                 else:
-    #                     f.write("%s, %s\n" %(key2, sec.psection()[key][key2]))
-    #         elif isinstance(sec.psection()[key], list):
-    #             for item in sec.psection()[key]:
-    #                 f.write("%s, %s\n" % (key, item))
-    #         elif isinstance(sec.psection()[key], set):
-    #             for item in list(sec.psection()[key]):
-    #                 f.write("%s, %s\n" % (key, item))
-    #         else:
-    #             f.write("%s, %s\n" % (key, sec.psection()[key]))
-    
-    
-    
+    section_order = [sec.name() for sec in cell.sections_rxd]
+    first_shells = [cell.shells[name][0] for name in section_order
+                    if "head" not in name]
+    camn_apic = uf.record_specie_vec(cell.camn, first_shells, 10)
+    camc_apic = uf.record_specie_vec(cell.camc, first_shells, 10)
+    calbca_apic = uf.record_specie_vec(cell.calbca, first_shells, 10)
+    fixed_apic = uf.record_specie_vec(cell.fixedca, first_shells, 10)
+    pmcaca_apic = uf.record_specie_vec(cell.pmca, cell.membrane_list, 10)
+    ncxca_apic = uf.record_specie_vec(cell.ncx, cell.membrane_list, 10)
     t = h.Vector().record(h._ref_t, 10)
     ca_apic = {}
     for sec in cell.sections:
@@ -56,6 +41,9 @@ if __name__ == "__main__":
     start = time.time()
     h.tstop = t_stop
     h.run(t_stop)
+
+
+    
     print(time.time() - start)
     ca_new = {}
     for sec_name in ca_apic.keys():
@@ -84,6 +72,66 @@ if __name__ == "__main__":
     ax8.set_xlabel("time (ms)")
     ax8.set_ylabel("dendrite (mm)")
     fig8.savefig("extracellular_Ca_dendrite.png")
+    camn_new = np.array([c.as_numpy()  for c in camn_apic])
+    camc_new = np.array([c.as_numpy()  for c in camc_apic])
+    calbca_new = np.array([c.as_numpy()  for c in calbca_apic])
+    fixed_new = np.array([c.as_numpy()  for c in fixed_apic])
+
+    pmcaca_new = np.array([c.as_numpy()  for c in pmcaca_apic])
+    ncxca_new = np.array([c.as_numpy()  for c in ncxca_apic])
+
+    fig1, ax1 = plt.subplots(1, 1)
+    show1 = ax1.imshow(pmcaca_new, extent=[0, t_stop, 0, 1000],
+                       origin="lower", interpolation="none", aspect="auto")
+    fig1.colorbar(show1)
+    ax1.set_title("pmcaCa membrane")
+    ax1.set_xlabel("time (ms)")
+    ax1.set_ylabel("dendrite (mm)")
+    fig1.savefig("pmcaca_membrane_dend.png")
+    fig3, ax3 = plt.subplots(1, 1)
+    show3 = ax3.imshow(ncxca_new, extent=[0, t_stop, 0, 1000], origin="lower",
+                       interpolation="none", aspect="auto")
+    fig3.colorbar(show3)
+    ax3.set_title("ncxCa membrane")
+    ax3.set_xlabel("time (ms)")
+    ax3.set_ylabel("dendrite (mm)")
+    fig3.savefig("ncxca_membrane.png")
+
+    fig5, ax5 = plt.subplots(1, 1)
+    show5 = ax5.imshow(fixed_new, extent=[0, t_stop, 0, 1000], origin="lower",
+                       interpolation="none", aspect="auto")
+    fig5.colorbar(show5)
+    ax5.set_title("Ca bound fixed")
+    ax5.set_xlabel("time (ms)")
+    ax5.set_ylabel("dendrite (mm)")
+    fig5.savefig("fixed_bound_dendrite.png")
+
+    fig9, ax9 = plt.subplots(1, 1)
+    show9 = ax9.imshow(camn_new, extent=[0, t_stop, 0, 1000], origin="lower",
+                       interpolation="none", aspect="auto")
+    fig9.colorbar(show9)
+    ax9.set_title("CalmodulinNCa cytosol")
+    ax9.set_xlabel("time (ms)")
+    ax9.set_ylabel("dendrite (mm)")
+    fig9.savefig("CaMN.png")
+
+
+    fig10, ax10 = plt.subplots(1, 1)
+    show10 = ax10.imshow(camc_new, extent=[0, t_stop, 0, 1000], origin="lower",
+                       interpolation="none", aspect="auto")
+    fig10.colorbar(show10)
+    ax10.set_title("CalmodulinCCa cytosol")
+    ax10.set_xlabel("time (ms)")
+    ax10.set_ylabel("dendrite (mm)")
+    fig10.savefig("CaMC.png")
     
+    fig11, ax11 = plt.subplots(1, 1)
+    show11 = ax11.imshow(calbca_new, extent=[0, t_stop, 0, 1000],
+                         origin="lower", interpolation="none", aspect="auto")
+    fig11.colorbar(show11)
+    ax11.set_title("Ca bound Calbindin cytosol")
+    ax11.set_xlabel("time (ms)")
+    ax11.set_ylabel("dendrite (mm)")
+    fig11.savefig("CalbindingCa.png")
 
     plt.show()
--- utility_functions.py
+++ utility_functions.py
@@ -1,26 +1,28 @@
 from neuron import h
 
-def record_specie_vec(specie, region, record_dt, spine=False, special_name=""):
+def record_specie_vec(specie, regions, record_dt, spine=False, special_name=""):
     '''use vec.record to record cyt calcium concentration. Will return list containing                                                                    
     vectors, one vector for each node'''
     Recordl = []
-
-    for mynode in specie[region].nodes:
-        if spine:
-            if "head" in mynode.sec.name():
-                cytcavec = h.Vector()
-                cytcavec.record(mynode._ref_concentration, record_dt)
-                Recordl.append(cytcavec)
-        else:
-            if "head" not in mynode.sec.name():
-                if not special_name:
-                    #print(mynode.segment, mynode.x, mynode.sec.trueparentseg())
+    if not isinstance(regions, list):
+        regions = [regions]
+    for region in regions:
+        for mynode in specie[region].nodes:
+            if spine:
+                if "head" in mynode.sec.name():
                     cytcavec = h.Vector()
                     cytcavec.record(mynode._ref_concentration, record_dt)
                     Recordl.append(cytcavec)
-                elif special_name in mynode.sec.name():
-                    cytcavec = h.Vector()
-                    cytcavec.record(mynode._ref_concentration, record_dt)
-                    Recordl.append(cytcavec)
+            else:
+                if "head" not in mynode.sec.name():
+                    if not special_name:
+                        #print(mynode.segment, mynode.x, mynode.sec.trueparentseg())
+                        cytcavec = h.Vector()
+                        cytcavec.record(mynode._ref_concentration, record_dt)
+                        Recordl.append(cytcavec)
+                    elif special_name in mynode.sec.name():
+                        cytcavec = h.Vector()
+                        cytcavec.record(mynode._ref_concentration, record_dt)
+                        Recordl.append(cytcavec)
 
     return Recordl
